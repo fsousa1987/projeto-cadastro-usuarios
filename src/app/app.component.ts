6848479f583e807884666dd0ab384087ff5a1390
Francisco Sousa <franciscoeds1987@gmail.com>
2025-01-08 09:22:16 -0300

    Criação do GenresService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {UsersService} from './services/users.service';
+import {GenresService} from './services/genres.service';
 
 @Component({
   selector: 'app-root',
@@ -10,19 +11,26 @@ import {UsersService} from './services/users.service';
 export class AppComponent implements OnInit {
 
   usersList: any = [];
+  genresList: any = [];
 
-  constructor(private readonly _usersService: UsersService) {
+  constructor(private readonly _usersService: UsersService,
+              private readonly _genresService: GenresService) {
   }
 
   ngOnInit(): void {
     this.getUsers();
+    this.getGenres();
   }
 
   private getUsers() {
     this._usersService.getUsers().subscribe((usersListResponse) => {
       this.usersList = usersListResponse;
-      console.log(usersListResponse);
     });
   }
 
+  private getGenres() {
+    this._genresService.getGenres().subscribe((genresListResponse) => {
+      this.genresList = genresListResponse;
+    });
+  }
 }
