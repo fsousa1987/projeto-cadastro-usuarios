8c4a2751339c34b75b4cae5265b308f8660ea684
Francisco Sousa <franciscoeds1987@gmail.com>
2025-01-10 14:30:41 -0300

    Criação da Barra de Progresso da Senha

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {GenresListResponse} from '../../types/genres-list-response';
 import {StatesListResponse} from '../../types/states-list-response';
 import {IUser} from '../../interfaces/user/user.interface';
+import {getPasswordStrengthValue} from '../../utils/get-password-strength-value';
 
 @Component({
   selector: 'app-user-form',
@@ -10,25 +11,24 @@ import {IUser} from '../../interfaces/user/user.interface';
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.scss'
 })
-export class UserFormComponent implements OnInit, OnChanges {
+export class UserFormComponent implements OnChanges {
+
+  passwordStrengthValue = 0;
 
   @Input() genresList: GenresListResponse = [];
   @Input() statesList: StatesListResponse = [];
   @Input() userSelected: IUser = {} as IUser;
 
-  ngOnInit(): void {
-    /*console.log('ngOnInit');
-    console.log('genresList', this.genresList);
-    console.log('statesList', this.statesList);
-    console.log('userSelected', this.userSelected);*/
-  }
-
   ngOnChanges(changes: SimpleChanges): void {
     console.log('ngOnChanges');
-    console.log('changes', changes);
-    /*console.log('genresList', this.genresList);
-    console.log('statesList', this.statesList);
-    console.log('userSelected', this.userSelected);*/
+    const USER_CHANGED = changes['userSelected'];
+
+    if (USER_CHANGED) {
+      this.onPasswordChange(this.userSelected.password);
+    }
   }
 
+  onPasswordChange(password: string) {
+    this.passwordStrengthValue = getPasswordStrengthValue(password);
+  }
 }
