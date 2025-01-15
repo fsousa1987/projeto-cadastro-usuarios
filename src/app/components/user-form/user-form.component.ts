1096f86c3ce3b1535821645caa1b830110c4d1c3
Francisco Sousa <franciscoeds1987@gmail.com>
2025-01-15 13:51:25 -0300

    Criação da Lógica para Focar no Campo Inválido

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {GenresListResponse} from '../../types/genres-list-response';
 import {StatesListResponse} from '../../types/states-list-response';
 import {IUser} from '../../interfaces/user/user.interface';
@@ -6,6 +6,7 @@ import {getPasswordStrengthValue} from '../../utils/get-password-strength-value'
 import {convertPtBrDateToDateObj} from '../../utils/convert-pt-br-date-to-date-obj';
 import {MatDatepickerInputEvent} from '@angular/material/datepicker';
 import {convertDateObjToPtBrDate} from '../../utils/convert-date-obj-to-pt-br-date';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -29,6 +30,11 @@ export class UserFormComponent implements OnInit, OnChanges {
   @Input() genresList: GenresListResponse = [];
   @Input() statesList: StatesListResponse = [];
   @Input() userSelected: IUser = {} as IUser;
+
+  @Output('onFormSubmit') onFormSubmitEmitter = new EventEmitter<void>();
+
+  constructor(private readonly _el: ElementRef) {
+  }
 
   ngOnInit(): void {
     this.setMinAndMaxDate();
@@ -76,6 +82,16 @@ export class UserFormComponent implements OnInit, OnChanges {
     return this.userSelected.musics.some(music => music.isFavorite);
   }
 
+  onFormSubmit(form: NgForm) {
+    if (form.invalid) {
+      this.focusOnInvalidControl(form);
+
+      return;
+    }
+
+    this.onFormSubmitEmitter.emit();
+  }
+
   private setMinAndMaxDate() {
     this.minDate = new Date(new Date().getFullYear() - 100, 0, 1);
     this.maxDate = new Date();
@@ -83,6 +99,18 @@ export class UserFormComponent implements OnInit, OnChanges {
 
   private setBirthDateToDatepicker(birthDate: string) {
     this.dateValue = convertPtBrDateToDateObj(birthDate);
+  }
+
+  private focusOnInvalidControl(form: NgForm) {
+    for (const control of Object.keys(form.controls)) {
+      if (form.controls[control].invalid) {
+        const invalidControl: HTMLElement = this._el.nativeElement.querySelector(`[name=${control}]`);
+
+        invalidControl.focus();
+
+        break;
+      }
+    }
   }
 
 }
